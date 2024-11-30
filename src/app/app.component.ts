c3bcf615c961fefbdea8d1c3a5db064c7b9665d4
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2024-11-30 12:46:25 +0530

    landing page styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MatButtonModule],
