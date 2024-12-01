701b8b64da0f6276db22989a5f4896c44952232d
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2024-12-01 11:27:01 +0530

    more work on landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
