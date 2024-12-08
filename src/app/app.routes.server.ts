476f056c6bbef09d748371a1ad31a9ea54e8d7f9
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2024-12-09 01:04:49 +0530

    temp. setting trip pages to ssr only

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { Server } from 'http';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'trip/:tripId',
+    renderMode: RenderMode.Server,
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
