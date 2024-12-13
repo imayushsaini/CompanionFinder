c91efb167b4ecb21a8d1fbb405351734a13c0e42
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2024-12-13 20:20:34 +0530

    trying adding angularFire auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,22 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "hop-on-trip",
+      appId: "1:1021695945294:web:04ed9a7d2b94c91042f11f",
+      storageBucket: "hop-on-trip.firebasestorage.app",
+      apiKey: "AIzaSyBOO-paUFQej_Ae4Szsk8cJ1ji2okkTpY8",
+      authDomain: "hop-on-trip.firebaseapp.com",
+      messagingSenderId: "1021695945294",
+      measurementId: "G-VZ85KL5GJ3"
+    })),
+    provideAuth(() => getAuth())]
 };
--- src/app/sigin-dialog/sigin-dialog.component.ts
+++ src/app/sigin-dialog/sigin-dialog.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, Optional } from '@angular/core';
+import { Auth, authState, signInAnonymously, signOut, User, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { traceUntilFirst } from '@angular/fire/performance';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { Subscription, Observable, EMPTY, map } from 'rxjs';
+
 @Component({
   selector: 'app-sigin-dialog',
   imports: [MatDialogModule, MatButtonModule],
@@ -8,5 +12,38 @@ import { MatDialogModule } from '@angular/material/dialog';
   styleUrl: './sigin-dialog.component.scss'
 })
 export class SiginDialogComponent {
+  private readonly userDisposable: Subscription|undefined;
+  public readonly user: Observable<User | null> = EMPTY;
 
+  showLoginButton = true;
+  showLogoutButton = false;
+  constructor(@Optional() private auth: Auth) {
+    console.log(auth)
+    if (auth) {
+      this.user = authState(this.auth);
+      this.userDisposable = authState(this.auth).pipe(
+        traceUntilFirst('auth'),
+        map(u => !!u)
+      ).subscribe((isLoggedIn: boolean) => {
+        this.showLoginButton = !isLoggedIn;
+        this.showLogoutButton = isLoggedIn;
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.userDisposable) {
+      this.userDisposable.unsubscribe();
+    }
+  }
+
+  async login() {
+    var gp = new GoogleAuthProvider();
+    // return await signInWithPopup(this.auth, gp);
+    return createUserWithEmailAndPassword(this.auth, "ayush@gmail.com", "123124242424")
+  }
+
+  async logout() {
+    return await signOut(this.auth);
+  }
 }
