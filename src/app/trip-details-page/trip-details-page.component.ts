193adea5283051ca8f5b641eb23f05a2afc3bc8e
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2024-12-19 19:46:07 +0530

    added back button on trip detail page

--- src/app/trip-details-page/trip-details-page.component.ts
+++ src/app/trip-details-page/trip-details-page.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
-import { NavBarComponent } from "../nav-bar/nav-bar.component";
-import { FooterComponent } from "../footer/footer.component";
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-trip-details-page',
-  imports: [MatRippleModule, NavBarComponent, FooterComponent],
-  templateUrl: './trip-details-page.component.html',
-  styleUrl: './trip-details-page.component.scss'
+    selector: 'app-trip-details-page',
+    imports: [MatRippleModule, NavBarComponent, FooterComponent, MatIconModule],
+    templateUrl: './trip-details-page.component.html',
+    styleUrl: './trip-details-page.component.scss',
 })
 export class TripDetailsPageComponent {
-
+    constructor(private router: Router) {}
+    onBackButtonPress() {
+        this.router.navigate(['/trips']);
+    }
 }
